f51c07666efbffe386c0d632236c0ed016448660
phucvndb123 <phucdx2204@gmail.com>
2025-07-23 10:39:23 +0700

    Create data.sql

---
+++ src/main/resources/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO books (title, author, genre, publish_year, status, borrow_count) VALUES
+('Lập trình Java', 'Nguyễn Văn A', 'CNTT', 2021, 'AVAILABLE', 5),
+('Clean Code', 'Robert C. Martin', 'Programming', 2019, 'BORROWED', 20),
+('Dế Mèn phiêu lưu ký', 'Tô Hoài', 'Văn học', 2020, 'AVAILABLE', 10),
+('Java Concurrency', 'Brian Goetz', 'Programming', 2018, 'AVAILABLE', 15),
+('Spring in Action', 'Craig Walls', 'Programming', 2022, 'BORROWED', 12);
